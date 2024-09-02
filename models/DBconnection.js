f86686559a58e887795cfe31417554c6f5976520
manzi-chriss <manzichrss6@gmail.com>
2024-09-02 21:52:40 +0200

    i changed the console.log for DB

--- models/DBconnection.js
+++ models/DBconnection.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
